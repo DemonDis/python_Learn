06e6230ed790de45d9b2921abdcc687d044515e4
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-11-01 19:43:21 +0700

    🚧 sse switch

--- back/sse.py
+++ back/sse.py
@@ -48,21 +48,20 @@ def rest_api():
     else:
         return 'Content-Type not supported'
     
+def sse_events():
+    # We are using a counter here for sending some value in the response
+    counter = 0
+    while True:
+        # In real world applications here we will fetch some data
+        # Remember to yield to continue to sending data
+        yield "data: {}\n\n".format(counter)
+        # Increase the counter for the next message
+        counter += 1
+        # Put a sleep for 2 second
+        time.sleep(2)
 
 @app.route('/sse')
 def sse():
-    def sse_events():
-        # We are using a counter here for sending some value in the response
-        counter = 0
-        while True:
-            # In real world applications here we will fetch some data
-            # Remember to yield to continue to sending data
-            yield "data: message counter - {}\n\n".format(counter)
-            # Increase the counter for the next message
-            counter += 1
-            # Put a sleep for 2 second
-            time.sleep(2)
-
     # Send back response
     return Response(sse_events(), mimetype="text/event-stream")
 if __name__ == "__main__":
--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [dataDB, setDataDB] = useState([]);
+  const [data, setData] = useState('Initializing...')
 
   useEffect(() => {
       fetch('http://127.0.0.1:3030/', {
@@ -15,14 +16,15 @@ function App() {
        })
        .then((response) => response.json())
        .then((data) => {
-          setData(data ? data.request.name : [])
+        setDataDB(data ? data.request.name : [])
        })
        .catch((error) => {
          console.error(error);
        });
     
   }, []);
-  const chekBack = () => {
+
+  const add_date = () => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -34,16 +36,38 @@ function App() {
       .then(response => response.json())
       .then(data => console.log(data));
   }
-
+  useEffect(() => {
+    const sse = new EventSource('http://127.0.0.1:3030/sse')
+    // const handleStream = (e) => { setData(e.data) }
+    // sse.onmessage = (e) =>{ handleStream(e) }
+    sse.onmessage = (e) => {
+      switch (e.data) {
+        case 'update_posts':
+          setData(e.data);
+          break;
+        case '5':
+          console.log('!!!!!!!')
+        default: setData(e.data);
+      }
+    }
+    sse.onerror = (e) => { sse.close() }
+    return () => { sse.close()
+    }
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => chekBack()}>CLICK</button>
+        <button onClick={() => add_date()}>CLICK</button>
         {
-          data.map((item, i) => {
+          dataDB.map((item, i) => {
             return <div key={i}>{item}</div>
          })
         }
+        <div>------</div>
+        The last streamed item was: {data}
+        {/* <button onClick={() => startSSE()}>Start SSE</button>
+        <button onClick={() => stopSSE()}>STOP SSE</button> */}
       </header>
     </div>
   );
